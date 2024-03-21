bee35b0685baf34cd16c63d9703ba3645060e413
André Marcello <160755361+eodede@users.noreply.github.com>
2024-03-21 17:39:48 -0300

    Add files via upload

---
+++ UmeCinquenta.java
@@ -0,0 +1,9 @@
+public class UmeCinquenta {
+    public static void main(String[] args) throws Exception {
+    for (int numero = 1; numero <= 50; numero++) {
+        if (numero % 2 != 0) {
+            System.out.println(numero);
+        }
+    }
+}
+}
